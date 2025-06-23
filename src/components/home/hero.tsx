2bc06ea038c653098f661d03fd9621c18ba6f87b
B1lol-dev <sardorovichgamer@gmail.com>
2025-06-23 20:25:56 +0500

    added hero

---
+++ src/components/home/hero.tsx
@@ -0,0 +1,22 @@
+import Container from "@/components/helpers/container";
+import { Button } from "../ui/button";
+
+const hero = () => {
+  return (
+    <section>
+      <Container className="flex justify-between">
+        <div>
+          <h1>Learn Any Foreign Language</h1>
+          <p>
+            With our teachers who write a program for each student, you will be
+            able to make your first sketch after the first lesson.
+          </p>
+          <Button>Get started</Button>
+        </div>
+        <div></div>
+      </Container>
+    </section>
+  );
+};
+
+export default hero;
